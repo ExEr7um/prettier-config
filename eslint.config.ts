625e222004958c20c48f777d7fad3f47b8e4a444
Vasily Kuzin <work@exer7um.com>
2025-03-04 19:11:56 +0300

    chore: Настройка ESLint

---
+++ eslint.config.ts
@@ -0,0 +1,1 @@
+export { default } from "@exer7um/eslint-config"
--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@ import type { Config } from "prettier"
 
 export default {
   htmlWhitespaceSensitivity: "ignore",
+  objectWrap: "collapse",
   plugins: [
     "prettier-plugin-css-order",
     "prettier-plugin-packagejson",
     "prettier-plugin-tailwindcss",
   ],
   semi: false,
-  objectWrap: "collapse",
 } as const satisfies Config

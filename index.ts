67dc93e63b2d06d9f1188843caaa4ae26722c0b1
Vasily Kuzin <work@exer7um.com>
2025-06-23 15:53:55 +0300

    feat: Плагин `@prettier/plugin-oxc`

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ export default {
   htmlWhitespaceSensitivity: "ignore",
   objectWrap: "collapse",
   plugins: [
+    "@prettier/plugin-oxc",
     "prettier-plugin-css-order",
     "prettier-plugin-packagejson",
     "prettier-plugin-tailwindcss",
